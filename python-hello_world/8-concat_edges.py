769e93a1baca88de56a161e0f93ce7994bf81796
Your Name <you@example.com>
2026-03-08 15:50:12 +0000

    salam

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-print(
-("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax")[36:61]
-+ ("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax")[61:66]
-+ ("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax")[:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[36:63] + str[63:68] + str[:6]
+print(str)
