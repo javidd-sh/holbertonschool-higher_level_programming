1f04ca41633723b3b7e511799fde5100c667f6f3
Adın Soyadın <email@example.com>
2026-03-08 11:52:25 +0000

    salam

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# This program prints a welcome message using str1 and str2
+# This file prints a concatenated string using str1 and str2
 str1 = "Welcome to"
 str2 = "Holberton School!"
 print(f"{str1} {str2}")
+
+✅ Açıklama:
