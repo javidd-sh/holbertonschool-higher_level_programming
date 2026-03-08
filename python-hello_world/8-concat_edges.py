eff43fef29aaa8b7a6ca81a1b015f0d6c50adbd0
Your Name <you@example.com>
2026-03-08 14:50:45 +0000

    salam

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str1 = "Python is an interpreted, interactive, object-oriented programming \
+language with a Python design philosophy"
+print(str1[39:70] + str1[71:76] + str1[77:83])
