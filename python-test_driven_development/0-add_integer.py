620588d74bb7c43f931f16167e89928ab14edb1e
Your Name <you@example.com>
2026-03-16 15:18:18 +0000

    salam

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Module that contains a function to add two integers."""
+"""Module for integer addition"""
 
 
 def add_integer(a, b=98):
-    """Adds two integers and returns the result.
+    """Adds two integers or floats and returns an integer"""
 
-    a and b must be integers or floats.
-    Floats are cast to integers before addition.
-    """
-
-    if not isinstance(a, (int, float)):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, (int, float)):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
     a = int(a)
