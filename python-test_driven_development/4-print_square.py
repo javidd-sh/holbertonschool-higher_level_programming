98a7d70e0cd23a96f10c71c3028a54413169d911
Your Name <you@example.com>
2026-03-18 14:26:29 +0000

    salam

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines a function to print a square with '#'."""
+
+
+def print_square(size):
+    """
+    Prints a square of size `size` using the character '#'.
+
+    Args:
+        size (int): Size length of the square
+
+    Raises:
+        TypeError: If size is not an integer
+        ValueError: If size is less than 0
+    """
+
+    # Type check
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    # Value check
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    # Print the square
+    for _ in range(size):
+        print("#" * size)
