112ddfbd3942d6e2cc9796788d65847fc8436180
Your Name <you@example.com>
2026-03-20 13:07:03 +0000

    salam

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,12 +6,15 @@ def say_my_name(first_name, last_name=""):
     """Prints 'My name is <first_name> <last_name>'.
 
     first_name and last_name must be strings.
+    last_name defaults to empty string.
     """
 
+    # Type checking
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
+    # Print formatted name
     print("My name is {} {}".format(first_name, last_name))
