9344514e880d6c4c80c1fe612f5f11e613400fe4
Your Name <you@example.com>
2026-03-22 09:50:13 +0000

    salam

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for Square class."""
+
+
+class Square:
+    """Defines a square by size."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
