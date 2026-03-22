9e499407e2d11d96a7b779d9c948f8486c95ded5
Your Name <you@example.com>
2026-03-22 10:32:39 +0000

    salam

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for inherits_from function."""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a class that inherited from a_class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if obj is an instance of a subclass of a_class,
+              False otherwise.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
