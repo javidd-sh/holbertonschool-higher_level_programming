2c403c7208aa97a9786247e6ba3884fc8d15ffef
Your Name <you@example.com>
2026-04-02 18:33:12 +0000

    salam

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
--- 1-create_user.sql
--- Create user user_0d_1 with password and grant all privileges
+-- Drop user if exists to ensure password is correct
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
+
+-- Create user with the required password
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
