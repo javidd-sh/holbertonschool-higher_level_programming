4de9446bcd77a5dae64b54d9f7c7260acfafe992
Your Name <you@example.com>
2026-04-02 18:23:18 +0000

    salam

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
--- Create user user_0d_1 with full privileges safely
+-- Create user user_0d_1 if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to the user
+-- Grant all privileges safely
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
