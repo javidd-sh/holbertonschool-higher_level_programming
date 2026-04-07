fa753480177e7d31dd809da53ad279b1b87c8033
Your Name <you@example.com>
2026-04-07 16:16:39 +0000

    salam

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
-
+"""Script that lists all states from the database hbtn_0e_0_usa."""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Get arguments
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Connect to MySQL
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=user,
-        passwd=password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    # Create cursor
     cursor = db.cursor()
-
-    # Execute query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch and print results
-    for row in cursor.fetchall():
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
-
-    # Close connections
     cursor.close()
     db.close()
