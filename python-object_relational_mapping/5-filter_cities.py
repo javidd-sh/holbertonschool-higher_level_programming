88a1269c341dcb643c9237249c9f3ebeeb74cc19
Your Name <you@example.com>
2026-04-07 16:38:35 +0000

    salam

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Lists all cities of a given state (safe from SQL injection)"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+
+    query = (
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
+    )
+
+    cursor.execute(query, (state_name,))
+
+    results = cursor.fetchall()
+
+    # Print as: city1, city2, city3
+    print(", ".join([row[0] for row in results]))
+
+    cursor.close()
+    db.close()
