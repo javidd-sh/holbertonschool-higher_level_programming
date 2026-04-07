32d57c6370a6c68ab5496b51937cd4492a6f7b8b
Your Name <you@example.com>
2026-04-07 16:35:35 +0000

    salam

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Safe filter states (prevents SQL injection)"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+
+    # SAFE query (no SQL injection possible)
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
