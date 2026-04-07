f59ef9b71deef107db4ec1ab31c2a38cde01bd58
Your Name <you@example.com>
2026-04-07 16:40:05 +0000

    salam

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""State class definition with SQLAlchemy ORM"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+# Base class for all tables
+Base = declarative_base()
+
+class State(Base):
+    """State class mapped to 'states' table"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
