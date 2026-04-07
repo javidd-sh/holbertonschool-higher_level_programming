e7bf1adbdd4d7e1d136fb2a93b701ce6b65fdb44
Your Name <you@example.com>
2026-04-07 16:54:57 +0000

    salam

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Add the State object "Louisiana" to the database"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    """Add Louisiana and print its id"""
+    if len(sys.argv) != 4:
+        return
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(user, password, db_name),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
