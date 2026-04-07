e5527101d561a25e54350b852d66367e60b7842d
Your Name <you@example.com>
2026-04-07 16:26:53 +0000

    salam

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""Filter states by user input (unsafe version)"""
-
+"""Script that filters states by user input from hbtn_0e_0_usa."""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=user,
-        passwd=password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
     cursor = db.cursor()
-
-    query = "SELECT * FROM states WHERE name = '{}' " \
-            "ORDER BY id ASC".format(state_name)
-    cursor.execute(query)
-
-    for row in cursor.fetchall():
+    cursor.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+        .format(sys.argv[4])
+    )
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
-
     cursor.close()
     db.close()
