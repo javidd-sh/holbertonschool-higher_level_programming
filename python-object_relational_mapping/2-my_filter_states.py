a3839e04ac35bab256b921c8a40817f6d328d03b
Your Name <you@example.com>
2026-04-07 16:29:22 +0000

    salam

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY states.id ASC"
         .format(sys.argv[4])
     )
     rows = cursor.fetchall()
