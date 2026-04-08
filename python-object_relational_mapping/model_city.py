c895ba5068f65c459bb80db936ac48792b8b9790
Your Name <you@example.com>
2026-04-08 13:20:28 +0000

    salam

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""City class definition linked to MySQL 'cities' table"""
+"""Module for City class definition."""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
 class City(Base):
-    """City class"""
-    __tablename__ = "cities"
+    """Defines a city linked to the cities table."""
+
+    __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
