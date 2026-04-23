e9b7e1908226eac387b8f20b16011e5df1a4b953
Your Name <you@example.com>
2026-04-23 14:34:20 +0000

    salam

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const header = document.querySelector('header');
+const redHeader = document.querySelector('#red_header');
+
+redHeader.addEventListener('click', function() {
+    header.classList.add('red');
+});
