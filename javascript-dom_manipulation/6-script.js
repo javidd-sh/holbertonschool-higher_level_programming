a7133e6adf34be24d290757d6842ef947c73433c
Your Name <you@example.com>
2026-04-23 14:38:09 +0000

    salam

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const characterDiv = document.querySelector('#character');
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    characterDiv.textContent = data.name;
+  });
