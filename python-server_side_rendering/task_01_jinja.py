f0a266f2d0420b20835e4da5a10f31e1f1178c87
Your Name <you@example.com>
2026-04-28 15:45:05 +0400

    a

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+template_dir = os.path.abspath('templates')
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def home():
